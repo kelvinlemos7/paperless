a7dc21fad512b5d99819eefe38f026304668b9c3
Kelvin Kauan <kelvinkauan17@gmail.com>
2026-02-18 23:25:37 -0300

    fix: adjust build for vercel

--- Cargo-Receiver/vite.config.ts
+++ Cargo-Receiver/vite.config.ts
@@ -3,30 +3,28 @@ import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 import path from "path"
 
-export default defineConfig(({ mode }) => ({
-  // Define /restaurante/ se for build do GitHub, senão usa a raiz /
-  base: mode === 'github' ? '/paperless/' : '/',
-  
-  root: path.resolve(__dirname, "client"),
-  
-  plugins: [
-    tailwindcss(), 
-    react()        
-  ],
-  
-  server: {
-    port: 5000,
-  },
-  
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "client/src"),
-    },
-  },
+export default defineConfig(({ mode }) => {
+  // Se estiver na Vercel, a base é '/'. Se for build 'github', é '/paperless/'
+  const baseRoute = process.env.VERCEL ? '/' : (mode === 'github' ? '/paperless/' : '/');
 
-  // Importante: Como o root é 'client', forçamos o build para a pasta dist na raiz do projeto
-  build: {
-    outDir: path.resolve(__dirname, "dist"),
-    emptyOutDir: true,
+  return {
+    base: baseRoute,
+    root: path.resolve(__dirname, "client"),
+    plugins: [
+      tailwindcss(), 
+      react()        
+    ],
+    server: {
+      port: 5000,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "client/src"),
+      },
+    },
+    build: {
+      outDir: path.resolve(__dirname, "dist"),
+      emptyOutDir: true,
+    }
   }
-}))
+})
